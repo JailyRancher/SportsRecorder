4ad6e199924cc573c2162b643cd8774beb43e7ce
YifangZhang <yifangzhang2009@gmail.com>
2016-03-26 11:10:58 -0500

    Screen On Touch Tracked in Log

--- SportsRecorder/app/src/main/java/com/example/jailyzeng/sportsrecorder/MainActivity.java
+++ SportsRecorder/app/src/main/java/com/example/jailyzeng/sportsrecorder/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.jailyzeng.sportsrecorder;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private float previousX;
+    private float previousY;
+    private int state;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        state = 0;
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        // MotionEvent reports input details from the touch screen
+        // and other input controls. In this case, you are only
+        // interested in events where the touch position changed.
+
+        float x = e.getX();
+        float y = e.getY();
+
+        switch (e.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                state = 1;
+            case MotionEvent.ACTION_UP:
+                if(state == 1){
+                    state = 0;
+                    previousX = x;
+                    previousY = y;
+                    Log.d("location", "location: X - " + Float.toString(previousX) + " || Y - " + Float.toString(previousY));
+                    return true;
+                }
+        }
+
+
+        return false;
+    }
+
+    public float getPreviousY() {
+        return previousY;
+    }
+
+    public float getPreviousX() {
+        return previousX;
     }
 }
